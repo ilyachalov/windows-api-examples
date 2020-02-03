41e4b18e588baba848e75ee3df6902589ee139da
Ilya Chalov <48008056+ilyachalov@users.noreply.github.com>
2020-02-03 10:16:41 +0300

    Add files via upload

--- output-to-printer/Output_to_printer_WinGDI_TextOut.cpp
+++ output-to-printer/Output_to_printer_WinGDI_TextOut.cpp
@@ -5,7 +5,7 @@
 // функций Windows GDI (graphics device interface — интерфейс графических
 // устройств)
 
-// для вывода строки на принтер используется функция TextOut
+// для вывода текста на устройство используется функция TextOut
 
 #include <io.h>      // для функции _setmode
 #include <fcntl.h>   // для константы _O_U16TEXT
@@ -14,8 +14,12 @@
 #include <windows.h> // для функций WinAPI
 using namespace std;
 
+// прототип функции для вывода текста на устройство
+BOOL TextOutput(HDC pDC, LPCWSTR str);
 // прототип функции для получения названия принтера по умолчанию
 BOOL GetDefPrinter(LPWSTR* printerName);
+// прототип функции для посылки задания на печать на принтер
+BOOL toPrinter(LPCWSTR str);
 
 int main()
 {
@@ -31,12 +35,60 @@ int main()
 	wostringstream ostr;
 	ostr << s1 << n1 << L"!";
 
+	// вызов функции для вывода строки на принтер по умолчанию
+	if (!toPrinter(ostr.str().c_str())) // если при вызове случилась ошибка
+		exit(-1);                       // завершаем программу ошибкой
+
+	wcout << L"Вывод файла на принтер выполнен успешно!\n";
+
+	// если принтер по умолчанию — виртуальный принтер Microsoft XPS Document Writer,
+	// то данные выводятся в файл «Распечатанный документ.xps» текущего каталога
+	// (у меня получалось, что текущий каталог — это каталог, в котором находится
+	// исполняемый файл программы)
+
+	return 0;
+}
+
+// функция для вывода текста на устройство
+BOOL TextOutput(HDC pDC, LPCWSTR str)
+{
+	BOOL OK = TextOutW(pDC, 100, 100, str, wcslen(str));
+	if (!OK)
+		return FALSE;
+
+	return TRUE;
+}
+
+// функция для получения названия принтера по умолчанию
+BOOL GetDefPrinter(LPWSTR* printerName)
+{
+	DWORD n; // количество символов (включая нулевой символ), требуемое под
+			 // массив-буфер для размещения названия принтера по умолчанию
+
+	// 1-й вызов: получим количество символов n
+	BOOL OK = GetDefaultPrinterW(NULL, &n);
+
+	// выделим память под массив-буфер для размещения названия принтера
+	*printerName = NULL;
+	if (n > 0)
+		*printerName = new WCHAR[n];
+
+	// 2-й вызов: получим название принтера по умолчанию
+	if (*printerName != NULL)
+		OK = GetDefaultPrinterW(*printerName, &n);
+
+	return OK;
+}
+
+// функция для посылки задания на печать на принтер
+BOOL toPrinter(LPCWSTR str)
+{
 	LPWSTR printerName; // для названия принтера по умолчанию
 	// вызываем функцию для получения названия принтера по умолчанию
 	if (!GetDefPrinter(&printerName))
 	{
 		wcout << L"Не удалось получить принтер по умолчанию!\n";
-		return -1; // завершаем программу ошибкой
+		return FALSE; // завершаем программу ошибкой
 	}
 
 	HDC printerDC; // контекст устройства (принтера)
@@ -67,11 +119,11 @@ int main()
 			if (result > 0)
 			{
 				// распечатать текст
-				BOOL OK = TextOutW(printerDC, 100, 100, ostr.str().c_str(), wcslen(ostr.str().c_str()));
+				BOOL OK = TextOutput(printerDC, str);
 				if (!OK)
 				{
 					wcout << L"Не получилось вывести на страницу текст!\n";
-					return -1;
+					return FALSE;
 				}
 
 				// закончить печать страницы
@@ -80,7 +132,7 @@ int main()
 			else
 			{
 				wcout << L"Не получилось начать печать страницы!\n";
-				return -1;
+				return FALSE;
 			}
 			// проинформируем спулер печати об окончании печати документа
 			EndDoc(printerDC);
@@ -88,9 +140,9 @@ int main()
 		else
 		{
 			wcout << L"Не получилось поставить новый документ в очередь печати!\n";
-			return -1;
+			return FALSE;
 		}
-		
+
 		// удалить контекст устройства (принтера), так как он нам больше не нужен
 		DeleteDC(printerDC);
 	}
@@ -98,31 +150,8 @@ int main()
 	else
 	{
 		wcout << L"Не удалось получить контекст устройства (принтера)!\n";
-		return -1;
+		return FALSE;
 	}
 
-	wcout << L"Документ распечатан успешно!\n";
-
-	return 0;
-}
-
-// функция для получения названия принтера по умолчанию
-BOOL GetDefPrinter(LPWSTR* printerName)
-{
-	DWORD n; // количество символов (включая нулевой символ), требуемое под
-			 // массив-буфер для размещения названия принтера по умолчанию
-
-	// 1-й вызов: получим количество символов n
-	BOOL OK = GetDefaultPrinterW(NULL, &n);
-
-	// выделим память под массив-буфер для размещения названия принтера
-	*printerName = NULL;
-	if (n > 0)
-		*printerName = new WCHAR[n];
-
-	// 2-й вызов: получим название принтера по умолчанию
-	if (*printerName != NULL)
-		OK = GetDefaultPrinterW(*printerName, &n);
-
-	return OK;
+	return TRUE;
 }
